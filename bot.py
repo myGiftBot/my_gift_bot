9533cbb47bcd2e61cc67dace9f7f9e68f96a85cc
myGiftBot <renat.tazitdinov+github.com@gmail.com>
2020-01-08 01:24:29 +0300

    repeat bot

--- bot.py
+++ bot.py
@@ -1,5 +1,18 @@
+# -*- coding: utf-8 -*-
+
 import telebot
-import requests
 import os
 
-TOKEN = os.environ.get("TOKEN")
+if 'TOKEN' in os.environ:
+    TOKEN = os.environ.get("TOKEN")
+else:
+    from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.infinity_polling()
