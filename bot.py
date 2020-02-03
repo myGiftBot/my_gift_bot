7910809ce42d9d0b6acbd20bcf9555aa6826172b
myGiftBot <renat.tazitdinov+github.com@gmail.com>
2020-02-03 21:43:21 +0300

    deploy

--- bot.py
+++ bot.py
@@ -2,41 +2,15 @@
 
 import telebot
 import os
-import time
 import flask
+import logging
 
 if 'TOKEN' in os.environ:
     TOKEN = os.environ.get("TOKEN")
-    WEBHOOK_HOST = 'https://my-gift-bot.herokuapp.com/'
-    WEBHOOK_PORT = int(os.environ.get("PORT", 5000))
-    WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
-    WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-    WEBHOOK_URL_PATH = "/%s/" % TOKEN
-
 else:
     from config import TOKEN
 
-    telebot.apihelper.proxy = {'https': 'socks5://211299130:Okrv20YQ@orbtl.s5.opennetwork.cc:999'}
-
 bot = telebot.TeleBot(TOKEN)
-app = flask.Flask(__name__)
-
-
-@app.route('/', methods=['GET', 'HEAD'])
-def index():
-    return ''
-
-
-@app.route(WEBHOOK_URL_PATH, methods=['POST'])
-def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        flask.abort(403)
-
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
@@ -51,11 +25,29 @@ def echo_message(message):
 
 
 if __name__ == '__main__':
-    if 'TOKE' in os.environ:
-        bot.remove_webhook()
-        time.sleep(0.1)
-        bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
-        app.run(host=WEBHOOK_LISTEN,
-                port=WEBHOOK_PORT)
+    if 'TOKEN' in os.environ:
+        logger = telebot.logger
+        telebot.logger.setLevel(logging.INFO)
+
+        server = flask.Flask(__name__)
+
+
+        @server.route("/bot", methods=['POST'])
+        def getMessage():
+            bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+            return "!", 200
+
+
+        @server.route("/")
+        def webhook():
+            bot.remove_webhook()
+            bot.set_webhook(
+                url=os.environ.get("APP_URL"))  # этот url нужно заменить на url вашего Хероку приложения
+            return "?", 200
+
+
+        server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
     else:
-        bot.infinity_polling()
+        telebot.apihelper.proxy = {'https': 'socks5://211299130:Okrv20YQ@orbtl.s5.opennetwork.cc:999'}
+        bot.remove_webhook()
+        bot.polling(none_stop=True)
